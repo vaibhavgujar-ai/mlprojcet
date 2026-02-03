4f661fc32888c4ec4985bd6fedeb2e19bb87a3c7
vaibhav gujar <am22d401@smail.iitm.ac.in>
2026-02-03 15:57:17 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import find_packages, setup
 from typing import List
 HYPEN_E_DOT = "-e ."
+
 def get_requirements(file_path:str)->List[str]:
+
     '''
     Docstring for get_requirements
     
@@ -10,10 +12,12 @@ def get_requirements(file_path:str)->List[str]:
     :return: Description
     :rtype: List[str]
     '''
+    
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n","") for req in requirements]
+
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
